220fde27ecb068ca41007d8dc122001386d5fc14
Drew Gonzales <drewgonzales360@gmail.com>
2016-05-15 02:59:10 -0700

    First commit

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,12 @@
+class PageController < ApplicationController
+  def landing
+  	render :layout => false;
+  end
+
+
+  def download
+  	send_data '/assets/data/DrewGonzalesResume.pdf',
+    :filename => "DrewGonzalesResume.pdf",
+    :type => "application/pdf"
+	end
+end
